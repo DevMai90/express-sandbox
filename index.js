176027bc3abe85cd5a2c5e5ffa730a904b025819
DevMai90 <dmai90@yahoo.com>
2019-03-23 21:50:44 -0700

    Connected server to public index.html file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Bring in express
 const express = require('express');
+// Bring in path module (it's a node module!!!)
+const path = require('path');
 
 // Initialize a variable with express
 const app = express();
@@ -7,8 +9,11 @@ const app = express();
 // Create a route - When we go to a webpage, it's a GET request
 // Takes in a request and response - Every route will have access to these two objects
 app.get('/', (req, res) => {
-  // res has this method. Sends something to the browser
-  res.send('<h1>Hello World</h1>');
+  // res has this method. Sends something to the browser. Not used too much
+  // res.send('<h1>Hello World!!</h1>');
+
+  // Transfer a file from the given path
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Look for environment variable called port
